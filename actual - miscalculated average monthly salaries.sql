831f6a363f1d8b50a3e054f5c5c682f83ff5855b
Sathyasree-Sundaravel <50878140+Sathyasree-Sundaravel@users.noreply.github.com>
2022-06-10 11:45:47 +0530

    Create actual - miscalculated average monthly salaries.sql

---
+++ actual - miscalculated average monthly salaries.sql
@@ -0,0 +1,8 @@
+/*
+Samantha was tasked with calculating the average monthly salaries for all employees in the EMPLOYEES table, but did not realize her keyboard's  key was broken until after completing the calculation. She wants your help finding the difference between her miscalculation (using salaries with any zeros removed), and the actual average salary.
+
+Write a query calculating the amount of error (i.e.: actual - miscalculated average monthly salaries), and round it up to the next integer.
+*/
+
+
+select ceil(avg(salary)-avg(replace(salary,0,''))) from employees;
